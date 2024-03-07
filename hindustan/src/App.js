eed2477db567506a47b9c2ef54ca13da6bbfec24
Pragathiswaran <mpragathiswaran@gmail.com>
2024-03-07 22:24:27 +0530

    commit

--- hindustan/src/App.js
+++ hindustan/src/App.js
@@ -3,6 +3,8 @@ import classes from './App.module.css';
 import NavBar from './Components/navbar/navbar';
 import SectionTitle from './Components/Sections/sectionTitle';
 import VideoDisplay from './Components/videoDisplay/videoDisplay';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 class App extends Component {
 state ={
   rolltext : {
@@ -15,7 +17,8 @@ state ={
 
   render() {
     return (
-      <div className={classes.App}>
+      <>
+        <div className={classes.App}>
           <div className={classes.NavBar}>
             <NavBar />
           </div>
@@ -25,7 +28,9 @@ state ={
           <div className={classes.SectionMiddle}>
           <VideoDisplay />
           </div>
+          <button className='btn btn-secondary'>Submit</button>
       </div>
+      </> 
     );
   }
 }
---
+++ hindustanuniv/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+function App() {
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default App;
